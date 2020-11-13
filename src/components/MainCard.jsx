1852af42fd95a4ffc091a088c8db90fcb66c1d4e
palakyadav1807 <palakyadav187@gmail.com>
2020-11-13 15:13:12 +0530

    adding functionality to main button

--- src/components/MainCard.jsx
+++ src/components/MainCard.jsx
@@ -1,12 +1,16 @@
-import React from "react";
+import React,{useState} from "react";
 import Button from "@material-ui/core/Button";
 
 function MainCard(props) {
+  const [reason,setReason]=useState(<img src={props.randomItem.img} alt="img" />);
+  function showReason(){
+    setReason((props.randomItem.list)[Math.floor(Math.random() *(props.randomItem.list).length)]);
+  }
   return (
     <div className="main-card">
       <h1>{props.randomItem.id.toUpperCase()}</h1>
-      <img src={props.randomItem.img} alt="img" />
-      <Button className="button">Find a Reason!</Button>
+      <div className="reason-block">{reason}</div>
+      <Button className="button" onClick={showReason}>Find a Reason!</Button>
     </div>
   );
 }
